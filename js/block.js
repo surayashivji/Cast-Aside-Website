170a2de462acb9e5ad3d68e472f8280569693a0c
Suraya Shivji <surayashivji@gmail.com>
2019-01-14 00:06:20 -0800

    progress on home

--- js/block.js
+++ js/block.js
@@ -1,15 +1,15 @@
-(function () {
+(function() {
     init();
     function init() {
         var blue = "#191970";
         var white = "#FFFFFF"
 
         //************************ Example 1 - reveal on load ********************************
-
+        
         var rev1 = new RevealFx(document.querySelector('#rev-1'), {
-            revealSettings: {
+            revealSettings : {
                 bgcolor: blue,
-                onCover: function (contentEl, revealerEl) {
+                onCover: function(contentEl, revealerEl) {
                     contentEl.style.opacity = 1;
                 }
             }
@@ -17,10 +17,10 @@
         rev1.reveal();
 
         var rev2 = new RevealFx(document.querySelector('#rev-2'), {
-            revealSettings: {
+            revealSettings : {
                 bgcolor: blue,
                 delay: 250,
-                onCover: function (contentEl, revealerEl) {
+                onCover: function(contentEl, revealerEl) {
                     contentEl.style.opacity = 1;
                 }
             }
@@ -28,64 +28,63 @@
         rev2.reveal();
 
         //************************ Example 2 - reveal on scroll ********************************
-
-        var rev3 = new RevealFx(document.querySelector('#rev-3'), {
-            revealSettings: {
-                bgcolor: blue,
-                onCover: function (contentEl, revealerEl) {
-                    contentEl.style.opacity = 1;
+        
+        var scrollElemToWatch_1 = document.getElementById('rev-3'),
+            watcher_1 = scrollMonitor.create(scrollElemToWatch_1, -300),				
+            rev3 = new RevealFx(scrollElemToWatch_1, {
+                revealSettings : {
+                    bgcolor: '#fcf652',
+                    direction: 'rl',
+                    onCover: function(contentEl, revealerEl) {
+                        contentEl.style.opacity = 1;
+                    }
                 }
-            }
-        });
-        rev3.reveal();
-
-        var rev4 = new RevealFx(document.querySelector('#rev-4'), {
-            revealSettings: {
-                bgcolor: blue,
-                delay: 250,
-                onCover: function (contentEl, revealerEl) {
-                    contentEl.style.opacity = 1;
+            }),
+            rev4 = new RevealFx(document.querySelector('#rev-4'), {
+                revealSettings : {
+                    bgcolor: '#7f40f1',
+                    delay: 250,
+                    onCover: function(contentEl, revealerEl) {
+                        contentEl.style.opacity = 1;
+                    }
                 }
-            }
-        });
-        rev4.reveal();
-
-        rev5 = new RevealFx(document.querySelector('#rev-5'), {
-            revealSettings: {
-                bgcolor: '#7f40f1',
-                delay: 500,
-                onCover: function (contentEl, revealerEl) {
-                    contentEl.style.opacity = 1;
+            }),
+            rev5 = new RevealFx(document.querySelector('#rev-5'), {
+                revealSettings : {
+                    bgcolor: '#7f40f1',
+                    delay: 500,
+                    onCover: function(contentEl, revealerEl) {
+                        contentEl.style.opacity = 1;
+                    }
                 }
-            }
-        }),
+            }),
 
             scrollElemToWatch_2 = document.getElementById('rev-6'),
             watcher_2 = scrollMonitor.create(scrollElemToWatch_2, -300),
             rev6 = new RevealFx(scrollElemToWatch_2, {
-                revealSettings: {
+                revealSettings : {
                     bgcolor: '#fcf652',
-                    onCover: function (contentEl, revealerEl) {
+                    onCover: function(contentEl, revealerEl) {
                         contentEl.style.opacity = 1;
                     }
                 }
             }),
             rev7 = new RevealFx(document.querySelector('#rev-7'), {
-                revealSettings: {
+                revealSettings : {
                     bgcolor: '#7f40f1',
                     direction: 'rl',
                     delay: 250,
-                    onCover: function (contentEl, revealerEl) {
+                    onCover: function(contentEl, revealerEl) {
                         contentEl.style.opacity = 1;
                     }
                 }
             }),
             rev8 = new RevealFx(document.querySelector('#rev-8'), {
-                revealSettings: {
+                revealSettings : {
                     bgcolor: '#7f40f1',
                     direction: 'rl',
                     delay: 500,
-                    onCover: function (contentEl, revealerEl) {
+                    onCover: function(contentEl, revealerEl) {
                         contentEl.style.opacity = 1;
                     }
                 }
@@ -94,46 +93,46 @@
             scrollElemToWatch_3 = document.getElementById('rev-9'),
             watcher_3 = scrollMonitor.create(scrollElemToWatch_3, -300),
             rev9 = new RevealFx(scrollElemToWatch_3, {
-                revealSettings: {
+                revealSettings : {
                     bgcolor: '#fcf652',
                     direction: 'rl',
-                    onCover: function (contentEl, revealerEl) {
+                    onCover: function(contentEl, revealerEl) {
                         contentEl.style.opacity = 1;
                     }
                 }
             }),
             rev10 = new RevealFx(document.querySelector('#rev-10'), {
-                revealSettings: {
+                revealSettings : {
                     bgcolor: '#7f40f1',
                     delay: 250,
-                    onCover: function (contentEl, revealerEl) {
+                    onCover: function(contentEl, revealerEl) {
                         contentEl.style.opacity = 1;
                     }
                 }
             }),
             rev11 = new RevealFx(document.querySelector('#rev-11'), {
-                revealSettings: {
+                revealSettings : {
                     bgcolor: '#7f40f1',
                     delay: 500,
-                    onCover: function (contentEl, revealerEl) {
+                    onCover: function(contentEl, revealerEl) {
                         contentEl.style.opacity = 1;
                     }
                 }
             });
 
-        watcher_1.enterViewport(function () {
+        watcher_1.enterViewport(function() {
             rev3.reveal();
             rev4.reveal();
             rev5.reveal();
             watcher_1.destroy();
         });
-        watcher_2.enterViewport(function () {
+        watcher_2.enterViewport(function() {
             rev6.reveal();
             rev7.reveal();
             rev8.reveal();
             watcher_2.destroy();
         });
-        watcher_3.enterViewport(function () {
+        watcher_3.enterViewport(function() {
             rev9.reveal();
             rev10.reveal();
             rev11.reveal();
@@ -141,73 +140,73 @@
         });
 
         //************************ Example 3 - api examples ********************************
-
+        
         var rev12 = new RevealFx(document.querySelector('#rev-12')),
             trigger_1 = document.getElementById('rev-trigger-1'),
             trigger_2 = document.getElementById('rev-trigger-2'),
             trigger_3 = document.getElementById('rev-trigger-3'),
             trigger_4 = document.getElementById('rev-trigger-4'),
             trigger_5 = document.getElementById('rev-trigger-5');
-
-        trigger_1.addEventListener('click', function () {
+        
+        trigger_1.addEventListener('click', function() {
             rev12.reveal({
                 bgcolor: '#c1c0b7',
                 duration: 300,
-                onStart: function (contentEl, revealerEl) { contentEl.style.opacity = 0; },
-                onCover: function (contentEl, revealerEl) { contentEl.style.opacity = 1; }
+                onStart: function(contentEl, revealerEl) { contentEl.style.opacity = 0; },
+                onCover: function(contentEl, revealerEl) { contentEl.style.opacity = 1; }
             });
         });
 
-        trigger_2.addEventListener('click', function () {
+        trigger_2.addEventListener('click', function() {
             rev12.reveal({
                 bgcolor: '#c1c0b7',
                 duration: 300,
                 direction: 'rl',
-                onStart: function (contentEl, revealerEl) { contentEl.style.opacity = 0; },
-                onCover: function (contentEl, revealerEl) { contentEl.style.opacity = 1; }
+                onStart: function(contentEl, revealerEl) { contentEl.style.opacity = 0; },
+                onCover: function(contentEl, revealerEl) { contentEl.style.opacity = 1; }
             });
         });
 
-        trigger_3.addEventListener('click', function () {
+        trigger_3.addEventListener('click', function() {
             rev12.reveal({
                 bgcolor: '#c1c0b7',
                 easing: 'easeOutExpo',
                 direction: 'bt',
-                onStart: function (contentEl, revealerEl) {
+                onStart: function(contentEl, revealerEl) { 
                     anime.remove(contentEl);
-                    contentEl.style.opacity = 0;
+                    contentEl.style.opacity = 0; 
                 },
-                onCover: function (contentEl, revealerEl) {
+                onCover: function(contentEl, revealerEl) { 
                     anime({
                         targets: contentEl,
                         duration: 800,
                         delay: 80,
                         easing: 'easeOutExpo',
-                        translateY: [40, 0],
-                        opacity: [0, 1]
+                        translateY: [40,0],
+                        opacity: [0,1]
                     });
                 }
             });
         });
 
-        trigger_4.addEventListener('click', function () {
+        trigger_4.addEventListener('click', function() {
             rev12.reveal({
                 bgcolor: '#c1c0b7',
                 duration: 300,
                 direction: 'tb',
-                onStart: function (contentEl, revealerEl) {
+                onStart: function(contentEl, revealerEl) { 
                     anime.remove(contentEl);
-                    contentEl.style.opacity = 0;
+                    contentEl.style.opacity = 0; 
                 },
-                onCover: function (contentEl, revealerEl) {
+                onCover: function(contentEl, revealerEl) { 
                     anime({
                         targets: contentEl,
                         duration: 500,
                         delay: 50,
                         easing: 'easeOutBounce',
-                        translateY: [-40, 0],
+                        translateY: [-40,0],
                         opacity: {
-                            value: [0, 1],
+                            value: [0,1],
                             duration: 300,
                             easing: 'linear'
                         }
@@ -216,24 +215,24 @@
             });
         });
 
-        trigger_5.addEventListener('click', function () {
+        trigger_5.addEventListener('click', function() {
             rev12.reveal({
                 bgcolor: '#7f40f1',
                 duration: 400,
                 easing: 'easeInOutQuad',
-                onStart: function (contentEl, revealerEl) {
+                onStart: function(contentEl, revealerEl) { 
                     anime.remove(contentEl);
-                    contentEl.style.opacity = 0;
+                    contentEl.style.opacity = 0; 
                 },
-                onCover: function (contentEl, revealerEl) {
-                    contentEl.style.opacity = 1;
+                onCover: function(contentEl, revealerEl) { 
+                    contentEl.style.opacity = 1; 
                     anime({
                         targets: contentEl,
                         duration: 800,
                         delay: 80,
                         easing: 'easeOutExpo',
-                        scale: [0.5, 1],
-                        opacity: [0, 1]
+                        scale: [0.5,1],
+                        opacity: [0,1]
                     });
                 }
             });
